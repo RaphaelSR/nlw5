dcbcbacc9def3e9277a60140385736a2b6a2c7cd
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-05-10 10:07:07 -0300

    Layout PlantSave

--- plantmanager/src/pages/PlantSave.tsx
+++ plantmanager/src/pages/PlantSave.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   View,
@@ -8,46 +8,81 @@ import {
   ScrollView,
   Platform,
   TouchableOpacity,
-  Button,
 } from "react-native";
+import { Button } from "../components/Button";
 import { SvgFromUri } from "react-native-svg";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import { useRoute } from "@react-navigation/core";
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker'
 import waterdrop from "../assets/waterdrop.png";
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+import { isBefore } from "date-fns";
+
+interface Params {
+  plant: {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frquency: {
+      times: number;
+      repeat_every: string;
+    };
+  }
+
+}
 
 export function PlantSave() {
+  const [selectedDateTime, SetSelectedDateTime] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(Platform.OS == 'ios');
+
+  const route = useRoute();
+  const { plant } = route.params as Params;
+
+  function handleChangeTime(event: Event, dateTime: Date | undefined){
+    if(Platform.OS == "android"){
+      setShowDatePicker(oldState => !oldState);
+    }
+
+    if(dateTime && isBefore(dateTime, new Date())){
+      SetSelectedDateTime(new Date());
+      return Alert.alert(`Escolha uma hora no futuro! ⏰`)
+    }
+
+    if(dateTime)
+      SetSelectedDateTime(dateTime);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.plantInfo}>
-        <SvgFromUri uri="" height={150} width={150} />
+        <SvgFromUri uri={plant.photo} height={150} width={150} />
 
-        <Text style={styles.plantName}>Nome da Planta</Text>
+        <Text style={styles.plantName}>{plant.name}</Text>
 
-        <Text style={styles.plantAbout}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, enim.
-          Aspernatur, blanditiis quas facilis quaerat, animi illo reiciendis,
-          exercitationem eveniet ad minima fuga soluta esse cupiditate iste amet
-          adipisci aliquam?
-        </Text>
+        <Text style={styles.plantAbout}>{plant.about}</Text>
       </View>
 
       <View style={styles.controller}>
         <View style={styles.tipContainer}>
           <Image source={waterdrop} style={styles.tipImage} />
-          <Text style={styles.tipText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            temporibus tenetur assumenda sed obcaecati placeat sapiente illo
-            facere totam sit quo eligendi sequi aut quod, molestiae nostrum
-            eius! Cum, deserunt.
-          </Text>
+          <Text style={styles.tipText}>{plant.water_tips}</Text>
         </View>
         <Text style={styles.alertLabel}>
-            Escolha o melhor horário para ser lembrado:
+          Escolha o melhor horário para ser lembrado:
         </Text>
 
-        <Button
-            title="Cadastrar planta"
-            onPress={() => {}}
+        <DateTimePicker 
+        value={selectedDateTime}
+        mode="time"
+        display="spinner"
+        onChange={handleChangeTime}
         />
+
+        <Button title="Cadastrar planta" onPress={() => {}} />
       </View>
     </View>
   );
@@ -56,15 +91,65 @@ export function PlantSave() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
     backgroundColor: colors.shape,
   },
-  plantName: {},
-  plantInfo: {},
-  plantAbout: {},
-  controller: {},
-  tipContainer: {},
-  tipImage: {},
-  tipText: {},
-  alertLabel: {},
+  plantInfo: {
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.shape,
+  },
+  controller: {
+    backgroundColor: colors.white,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: getBottomSpace() || 20,
+  },
+  plantName: {
+    fontFamily: fonts.heading,
+    fontSize: 24,
+    color: colors.heading,
+    marginTop: 15,
+  },
+
+  plantAbout: {
+    textAlign: "center",
+    fontFamily: fonts.text,
+    color: colors.heading,
+    fontSize: 17,
+    marginTop: 10,
+  },
+
+  tipContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: colors.blue_light,
+    padding: 20,
+    borderRadius: 20,
+    position: "relative",
+    bottom: 60,
+  },
+  tipImage: {
+    width: 56,
+    height: 56,
+  },
+  tipText: {
+    flex: 1,
+    marginLeft: 20,
+    fontFamily: fonts.text,
+    color: colors.blue,
+    fontSize: 17,
+    textAlign: "justify",
+  },
+  alertLabel: {
+    textAlign: "center",
+    fontFamily: fonts.complement,
+    color: colors.heading,
+    fontSize: 12,
+    marginBottom: 5,
+  },
 });
--- plantmanager/src/pages/PlantSelect.tsx
+++ plantmanager/src/pages/PlantSelect.tsx
@@ -76,7 +76,7 @@ export function PlantSelect() {
   }
 
   function handlePlantSelect(plant: PlantProps){
-    navigation.navigate('PlantSave')
+    navigation.navigate('PlantSave', {plant});
   }
 
   useEffect(() => {
